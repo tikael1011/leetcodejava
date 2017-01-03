70d9b6161e98936eca514345abece4b149e534df
tikael1011 <tikael1011@users.noreply.github.com>
2017-01-03 08:32:59 -0500

    Easy

--- 191. Number of 1 Bits.java
+++ 191. Number of 1 Bits.java
@@ -28,7 +28,6 @@ public class Solution{
         return bits;
     }
 }
-
 //2ms
 
 
@@ -57,17 +56,3 @@ public class Solution {
     }
 }
 
-
-public class Solution{
-    public static int hammingWeight(int n) {
-    	int bits = 0;
-        int mask = 1;
-        for (int i = 0; i < 32; i++) {
-            if ((n & mask) != 0) {
-                bits++;
-            }
-            mask <<= 1;
-        }
-        return bits;
-    }
-}
