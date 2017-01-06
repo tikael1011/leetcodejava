ded9931afd1076c5c8da88838434169bb76ad89d
tikael1011 <tikael1011@users.noreply.github.com>
2017-01-06 18:50:37 -0500

    Easy

--- 344. Reverse String.java
+++ 344. Reverse String.java
@@ -14,7 +14,7 @@ public class Solution {
     public String reverseString(String s) {
         String res="";
         for(int i = s.length()-1; i>=0;i--){
-            res+=s.charAt(i);
+            res+=s.charAt(i); //string concat is expensive!!
         }
         return res;
     }
