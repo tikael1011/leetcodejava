e25ac9a6a47fc42e7b13160fa4b363c1b4bb78f3
tikael1011 <tikael1011@users.noreply.github.com>
2017-01-09 11:38:26 -0500

    Easy

--- 223. Rectangle Area.java
+++ 223. Rectangle Area.java
@@ -5,3 +5,15 @@ Each rectangle is defined by its bottom left corner and top right corner as show
 Rectangle Area
 Assume that the total area is never beyond the maximum possible value of int.
 */
+
+
+//5ms
+//forget about tons of conditional branches ,no need to do that.
+
+public class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int left = Math.max(A,E), right = Math.max(Math.min(C,G), left);
+        int bottom = Math.max(B,F), top = Math.max(Math.min(D,H), bottom);
+        return (C-A)*(D-B) - (right-left)*(top-bottom) + (G-E)*(H-F);
+    }
+}
