d44031f68478383068a1a8230f61c0071ee3303f
tikael1011 <tikael1011@users.noreply.github.com>
2017-01-16 16:55:44 -0500

    Easy[i]

--- 401. Binary Watch.java
+++ 401. Binary Watch.java
@@ -23,3 +23,59 @@ The minute must be consist of two digits and may contain a leading zero, for exa
 gm
 
 */
+
+// I do not know Integer.bitCount method at all...
+//my original thought is too weired..
+
+/*****The others solution *****/
+
+//https://leetcode.com/submissions/detail/89444317/ not found??
+
+public List<String> readBinaryWatch(int num) {
+    List<String> times = new ArrayList<>();
+    for (int h=0; h<12; h++)
+        for (int m=0; m<60; m++)
+            if (Integer.bitCount(m) + Integer.bitCount(h) == num) // can be Integer.bitCount(m + (h << 6))
+              // since the max m is 59,and uses 5 bits. or can be Integer.bitCount(h * 64 + m)
+                //times.add(String.format("%d:%02d", h, m)); is easy to understand but slower
+                times.add(h + (m < 10 ? ":0" : ":") + m);
+    return times;        
+}
+
+// or use backtracking and "permutation and combination"
+
+public class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<>();
+        int[] nums1 = new int[]{8, 4, 2, 1}, nums2 = new int[]{32, 16, 8, 4, 2, 1};
+        for(int i = 0; i <= num; i++) {
+            List<Integer> list1 = generateDigit(nums1, i);
+            List<Integer> list2 = generateDigit(nums2, num - i);
+            for(int num1: list1) {
+                if(num1 >= 12) continue;
+                for(int num2: list2) {
+                    if(num2 >= 60) continue;
+                    res.add(num1 + ":" + (num2 < 10 ? "0" + num2 : num2));
+                }
+            }
+        }
+        return res;
+    }
+
+    private List<Integer> generateDigit(int[] nums, int count) {
+        List<Integer> res = new ArrayList<>();
+        generateDigitHelper(nums, count, 0, 0, res);
+        return res;
+    }
+
+    private void generateDigitHelper(int[] nums, int count, int pos, int sum, List<Integer> res) {
+        if(count == 0) {
+            res.add(sum);
+            return;
+        }
+        
+        for(int i = pos; i < nums.length; i++) {
+            generateDigitHelper(nums, count - 1, i + 1, sum + nums[i], res);    
+        }
+    }
+}
