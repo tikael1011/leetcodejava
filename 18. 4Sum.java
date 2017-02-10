a673c382fbdb09d162da837888e0f95ff4b066dc
tikael1011 <tikael1011@users.noreply.github.com>
2017-02-10 03:08:05 -0500

    Medium[i]

---
+++ 18. 4Sum.java
@@ -0,0 +1,272 @@
+/*
+Given an array S of n integers, are there elements a, b, c,
+and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note: The solution set must not contain duplicate quadruplets.
+
+For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+
+A solution set is:
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+*/
+
+
+//O(n**3)
+//87ms
+//27.91%
+
+public class Solution {
+    public List<List<Integer>> fourSum(int[] num, int target) {
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        if(num.length<4)return ans;
+        Arrays.sort(num);
+        for(int i=0; i<num.length-3; i++){
+            if(i>0&&num[i]==num[i-1])continue;
+            for(int j=i+1; j<num.length-2; j++){
+                if(j>i+1&&num[j]==num[j-1])continue;
+                int low=j+1, high=num.length-1;
+                while(low<high){
+                    int sum=num[i]+num[j]+num[low]+num[high];
+                    if(sum==target){
+                        ans.add(Arrays.asList(num[i], num[j], num[low], num[high]));
+                        while(low<high&&num[low]==num[low+1])low++;
+                        while(low<high&&num[high]==num[high-1])high--;
+                        low++;
+                        high--;
+                    }
+                    else if(sum<target)low++;
+                    else high--;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
+// reduce to 3 sum, reduce to 2sum
+//28ms
+//95.35%, Best!!
+
+public List<List<Integer>> fourSum(int[] nums, int target) {
+		ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+		int len = nums.length;
+		if (nums == null || len < 4)
+			return res;
+
+		Arrays.sort(nums);
+
+		int max = nums[len - 1];
+		if (4 * nums[0] > target || 4 * max < target)
+			return res;
+
+		int i, z;
+		for (i = 0; i < len; i++) {
+			z = nums[i];
+			if (i > 0 && z == nums[i - 1])// avoid duplicate
+				continue;
+			if (z + 3 * max < target) // z is too small
+				continue;
+			if (4 * z > target) // z is too large
+				break;
+			if (4 * z == target) { // z is the boundary
+				if (i + 3 < len && nums[i + 3] == z)
+					res.add(Arrays.asList(z, z, z, z));
+				break;
+			}
+
+			threeSumForFourSum(nums, target - z, i + 1, len - 1, res, z);
+		}
+
+		return res;
+	}
+
+	/*
+	 * Find all possible distinguished three numbers adding up to the target
+	 * in sorted array nums[] between indices low and high. If there are,
+	 * add all of them into the ArrayList fourSumList, using
+	 * fourSumList.add(Arrays.asList(z1, the three numbers))
+	 */
+	public void threeSumForFourSum(int[] nums, int target, int low, int high, ArrayList<List<Integer>> fourSumList,
+			int z1) {
+		if (low + 1 >= high)
+			return;
+
+		int max = nums[high];
+		if (3 * nums[low] > target || 3 * max < target)
+			return;
+
+		int i, z;
+		for (i = low; i < high - 1; i++) {
+			z = nums[i];
+			if (i > low && z == nums[i - 1]) // avoid duplicate
+				continue;
+			if (z + 2 * max < target) // z is too small
+				continue;
+
+			if (3 * z > target) // z is too large
+				break;
+
+			if (3 * z == target) { // z is the boundary
+				if (i + 1 < high && nums[i + 2] == z)
+					fourSumList.add(Arrays.asList(z1, z, z, z));
+				break;
+			}
+
+			twoSumForFourSum(nums, target - z, i + 1, high, fourSumList, z1, z);
+		}
+
+	}
+
+	/*
+	 * Find all possible distinguished two numbers adding up to the target
+	 * in sorted array nums[] between indices low and high. If there are,
+	 * add all of them into the ArrayList fourSumList, using
+	 * fourSumList.add(Arrays.asList(z1, z2, the two numbers))
+	 */
+	public void twoSumForFourSum(int[] nums, int target, int low, int high, ArrayList<List<Integer>> fourSumList,
+			int z1, int z2) {
+
+		if (low >= high)
+			return;
+
+		if (2 * nums[low] > target || 2 * nums[high] < target)
+			return;
+
+		int i = low, j = high, sum, x;
+		while (i < j) {
+			sum = nums[i] + nums[j];
+			if (sum == target) {
+				fourSumList.add(Arrays.asList(z1, z2, nums[i], nums[j]));
+
+				x = nums[i];
+				while (++i < j && x == nums[i]) // avoid duplicate
+					;
+				x = nums[j];
+				while (i < --j && x == nums[j]) // avoid duplicate
+					;
+			}
+			if (sum < target)
+				i++;
+			if (sum > target)
+				j--;
+		}
+		return;
+	}
+  
+  
+  //othermethod
+  /*
+  To avoid duplicate list items, I skip unnecessary indices at two locations:
+
+one at the end of the outer loop (i-loop)
+the other at the end of the inner loop (j-loop).
+To avoid useless computations, the following is kind of critical:
+
+the function return immediately when nums[i]*4 > target
+the inner loop break immediately when nums[j]*4 < target.
+  */
+  //30ms
+  //93.42%
+  
+  
+  public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        int second = 0, third = 0, nexti = 0, nextj = 0;
+        for(int i=0, L=nums.length; i<L-3; i++) {
+            if(nums[i]<<2 > target) return list; // return immediately
+            for(int j=L-1; j>i+2; j--) {
+                if(nums[j]<<2 < target) break; // break immediately
+                int rem = target-nums[i]-nums[j];
+                int lo = i+1, hi=j-1;
+                while(lo<hi) {
+                    int sum = nums[lo] + nums[hi];
+                    if(sum>rem) --hi;
+                    else if(sum<rem) ++lo;
+                    else {
+                        list.add(Arrays.asList(nums[i],nums[lo],nums[hi],nums[j]));
+                        while(++lo<=hi && nums[lo-1]==nums[lo]) continue; // avoid duplicate results
+                        while(--hi>=lo && nums[hi]==nums[hi+1]) continue; // avoid duplicate results
+                    }
+                }
+                while(j>=1 && nums[j]==nums[j-1]) --j; // skip inner loop
+            }
+            while(i<L-1 && nums[i]==nums[i+1]) ++i; // skip outer loop
+        }
+        return list;
+    }
+}
+
+/*
+If you have already read and implement the 3sum and 4sum by using the sorting approach:
+reduce them into 2sum at the end, you might already got the feeling that,
+all ksum problem can be divided into two problems:
+
+2sum Problem
+Reduce K sum problem to K – 1 sum Problem
+Therefore, the ideas is simple and straightforward. 
+We could use recursive to solve this problem. Time complexity is O(N^(K-1)).
+*/
+
+//136ms
+
+public class Solution {
+        int len = 0;
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            len = nums.length;
+            Arrays.sort(nums);
+            return kSum(nums, target, 4, 0);
+        }
+       private ArrayList<List<Integer>> kSum(int[] nums, int target, int k, int index) {
+            ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+            if(index >= len) {
+                return res;
+            }
+            if(k == 2) {
+            	int i = index, j = len - 1;
+            	while(i < j) {
+                    //find a pair
+            	    if(target - nums[i] == nums[j]) {
+            	    	List<Integer> temp = new ArrayList<>();
+                    	temp.add(nums[i]);
+                    	temp.add(target-nums[i]);
+                        res.add(temp);
+                        //skip duplication
+                        while(i<j && nums[i]==nums[i+1]) i++;
+                        while(i<j && nums[j-1]==nums[j]) j--;
+                        i++;
+                        j--;
+                    //move left bound
+            	    } else if (target - nums[i] > nums[j]) {
+            	        i++;
+                    //move right bound
+            	    } else {
+            	        j--;
+            	    }
+            	}
+            } else{
+                for (int i = index; i < len - k + 1; i++) {
+                    //use current number to reduce ksum into k-1sum
+                    ArrayList<List<Integer>> temp = kSum(nums, target - nums[i], k-1, i+1);
+                    if(temp != null){
+                        //add previous results
+                        for (List<Integer> t : temp) {
+                            t.add(0, nums[i]);
+                        }
+                        res.addAll(temp);
+                    }
+                    while (i < len-1 && nums[i] == nums[i+1]) {
+                        //skip duplicated numbers
+                        i++;
+                    }
+                }
+            }
+            return res;
+        }
+    }
