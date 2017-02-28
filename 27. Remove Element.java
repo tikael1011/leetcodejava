0ddc454af62af06cd6602ef4d9f9d25a20c5c5f4
tikael1011 <tikael1011@users.noreply.github.com>
2017-02-28 16:28:41 -0500

    Easy

--- 27. Remove Element.java
+++ 27. Remove Element.java
@@ -36,3 +36,18 @@ public class Solution {
         return len;
     }
 }
+
+
+//even better
+
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        int l = A.length;
+        for (int i=0; i<l; i++) {
+            if (A[i] == elem) {
+                A[i--] = A[l-- -1];
+            }
+        }
+        return l;
+    }
+}
