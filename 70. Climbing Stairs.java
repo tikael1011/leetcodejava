11b0074307c8de5d5904be38d1fe04249580e7f5
tikael1011 <tikael1011@users.noreply.github.com>
2017-05-20 00:55:59 -0400

    Easy

--- 70. Climbing Stairs.java
+++ 70. Climbing Stairs.java
@@ -7,6 +7,7 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 Note: Given n will be a positive integer.
 */
 
+
 public int climbStaris(int n){
   int p =1;
   int q = 1;
