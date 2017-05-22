ceb3b42e5f9b3b05c35402628feacf394eedd0f6
tikael1011 <tikael1011@users.noreply.github.com>
2017-05-22 16:02:05 -0400

    Medium
    backtrack,DFS

--- 22. Generate Parentheses.java
+++ 22. Generate Parentheses.java
@@ -11,3 +11,45 @@ For example, given n = 3, a solution set is:
   "()()()"
 ]
 */
+
+
+
+
+ public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+    
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+        
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    }
+
+
+public List<String> generateParenthesis(int n) {
+	List<String> result = new ArrayList<String>();
+	if (n == 0) {
+		result.add("");
+	} else {
+		for (int i = n - 1; i >= 0; i--) {
+			List<String> insertSub = generateParenthesis(i);
+			List<String> tailSub = generateParenthesis(n - 1 - i);
+			for (String insert : insertSub) {
+				for (String tail : tailSub) {
+					result.add("(" + insert + ")" + tail);
+				}
+			}
+
+		}
+	}
+	return result;
+}
