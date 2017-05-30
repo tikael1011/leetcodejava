31eb539eace929cebfc886d8681a44b97602e3e4
tikael1011 <tikael1011@users.noreply.github.com>
2017-05-29 23:59:19 -0400

    Medium
    dp && other

--- 152. Maximum Product Subarray.java
+++ 152. Maximum Product Subarray.java
@@ -5,3 +5,48 @@ Find the contiguous subarray within an array (containing at least one number) wh
 For example, given the array [2,3,-2,4],
 the contiguous subarray [2,3] has the largest product = 6.
 */
+
+// no need to worry about stack overflow, why??
+
+// O(n) rc, O(n) sc.
+
+public class Solution {
+    public int maxProduct(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int max = A[0], min = A[0], result = A[0];
+        for (int i = 1; i < A.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
+            min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
+    }
+}
+
+
+//O(n) rc, O(1) sc
+
+public int maxProduct(int[] A) {
+    if (A.length == 0) {
+        return 0;
+    }
+    
+    int maxherepre = A[0];
+    int minherepre = A[0];
+    int maxsofar = A[0];
+    int maxhere, minhere;
+    
+    for (int i = 1; i < A.length; i++) {
+        maxhere = Math.max(Math.max(maxherepre * A[i], minherepre * A[i]), A[i]);
+        minhere = Math.min(Math.min(maxherepre * A[i], minherepre * A[i]), A[i]);
+        maxsofar = Math.max(maxhere, maxsofar);
+        maxherepre = maxhere;
+        minherepre = minhere;
+    }
+    return maxsofar;
+}
