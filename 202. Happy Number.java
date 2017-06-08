2b0e6832d7ed9121475b79ac35d313399ce77a11
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-08 19:51:34 -0400

    Easy
    Funny math

--- 202. Happy Number.java
+++ 202. Happy Number.java
@@ -13,3 +13,76 @@ Example: 19 is a happy number
 62 + 82 = 100
 12 + 02 + 02 = 1
 */
+
+
+/**
+No need to worry about stakcoverflow?  why?
+**/
+
+// use observation, math
+
+public boolean isHappy(int n) {
+        if(n<=0) return false;
+        while(true){
+            int sum=0;
+            while(n!=0){
+              sum+=(n%10)*(n%10);
+              n=n/10;
+            }
+            if(sum/10==0){
+               if(sum==1||sum==7) return true;
+               else return false;
+            }
+            n=sum;
+        }
+    }
+
+//use hashset
+
+
+public boolean isHappy(int n) {
+    Set<Integer> inLoop = new HashSet<Integer>();
+    int squareSum,remain;
+	while (inLoop.add(n)) {
+		squareSum = 0;
+		while (n > 0) {
+		    remain = n%10;
+			squareSum += remain*remain;
+			n /= 10;
+		}
+		if (squareSum == 1)
+			return true;
+		else
+			n = squareSum;
+
+	}
+	return false;
+
+}
+
+//other thoughts (no methods at all, lol)
+
+public class Solution {
+    public boolean isHappy(int n) {
+        int x = n;
+        int y = n;
+        while(x>1){
+            x = cal(x) ;
+            if(x==1) return true ;
+            y = cal(cal(y));
+            if(y==1) return true ;
+
+            if(x==y) return false;
+        }
+        return true ;
+    }
+    public int cal(int n){
+        int x = n;
+        int s = 0;
+        while(x>0){
+            s = s+(x%10)*(x%10);
+            x = x/10;
+        }
+        return s ;
+    }
+}
