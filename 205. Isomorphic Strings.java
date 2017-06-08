4da53aa0884bfd1f9398b6f40c3aafcc1291f03b
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-08 01:58:26 -0400

    Easy

--- 205. Isomorphic Strings.java
+++ 205. Isomorphic Strings.java
@@ -94,4 +94,21 @@ public class Solution {
     	return true;
     }
 }
-  
+//be ware the following version,
+
+public boolean isIsomorphic(String s, String t) {
+    int l = s.length();
+    int[] m1 = new int[256];
+    int[] m2 = new int[256];
+    //initialize to avoid default '0'
+    for (int i = 0; i < 256; i++) {
+        m1[i] = m2[i] = -1;
+    }
+    /* following logic
+    for (int i = 0; i < len; i++) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = m2[t[i]] = i;
+        }
+        return true;
+    */
+}
