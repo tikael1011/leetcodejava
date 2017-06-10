d45e85d443b974a7f552a1c1634d2413c372b9b0
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-09 22:01:02 -0400

    Easy
    fast and slow 'pointer'

--- 141. Linked List Cycle.java
+++ 141. Linked List Cycle.java
@@ -4,3 +4,36 @@ Given a linked list, determine if it has a cycle in it.
 Follow up:
 Can you solve it without using extra space?
 */
+
+
+//https://en.wikipedia.org/wiki/Cycle_detection#Tortoise_and_hare
+
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next == null) return false;
+        ListNode walker = head;
+        ListNode runner = head.next;
+        while(runner.next!=null && runner.next.next!=null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker==runner) return true;
+        }
+        return false;
+    }
+}
+
+
+//or just 
+
+public boolean hasCycle(ListNode head) {
+    if(head==null) return false;
+    ListNode walker = head;
+    ListNode runner = head;
+    while(runner.next!=null && runner.next.next!=null) {
+        walker = walker.next;
+        runner = runner.next.next;
+        if(walker==runner) return true;
+    }
+    return false;
+}
