c437c3355a446e982426ffbc73b5734a582eaed5
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-13 06:58:41 -0400

    Medium
    math

--- 36. Valid Sudoku.java
+++ 36. Valid Sudoku.java
@@ -3,3 +3,45 @@ Determine if a Sudoku is valid,
 The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
 A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
 */
+
+
+/**
+The key of this problem is to 'discvoer' %3 and /3,
+Otherwise code can be chambersum.
+*/
+
+public boolean isValidSudoku(char[][] board) {
+    for(int i = 0; i<9; i++){
+        HashSet<Character> rows = new HashSet<Character>();
+        HashSet<Character> columns = new HashSet<Character>();
+        HashSet<Character> cube = new HashSet<Character>();
+        for (int j = 0; j < 9;j++){
+            if(board[i][j]!='.' && !rows.add(board[i][j]))
+                return false;
+            if(board[j][i]!='.' && !columns.add(board[j][i]))
+                return false;
+            int RowIndex = 3*(i/3);
+            int ColIndex = 3*(i%3);
+            if(board[RowIndex + j/3][ColIndex + j%3]!='.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
+                return false;
+        }
+    }
+    return true;
+}
+
+//shorter and more readable version
+
+public boolean isValidSudoku(char[][] board) {
+    Set seen = new HashSet();
+    for (int i=0; i<9; ++i) {
+        for (int j=0; j<9; ++j) {
+            char number = board[i][j];
+            if (number != '.')
+                if (!seen.add(number + " in row " + i) ||
+                    !seen.add(number + " in column " + j) ||
+                    !seen.add(number + " in block " + i/3 + "-" + j/3))
+                    return false;
+        }
+    }
+    return true;
+}
