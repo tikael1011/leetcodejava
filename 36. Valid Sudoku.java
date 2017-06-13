dda3d268944e37057e3299b9c89eba43e6045a64
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-12 23:29:19 -0400

    Create 36. Valid Sudoku.java

---
+++ 36. Valid Sudoku.java
@@ -0,0 +1,5 @@
+/*
+Determine if a Sudoku is valid,
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+*/
