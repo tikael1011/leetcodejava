8c345dc6eb8137c2d887fc78a36473a60f5cbb68
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-14 23:01:42 -0400

    Easy

--- 217. Contains Duplicate.java
+++ 217. Contains Duplicate.java
@@ -1,7 +1,16 @@
 /*
 check if there is duplicate integer in an unsorted array
 */
-
+/*
 1. rc O(n^2) ,brutal force, sc O(1)
 2. sort the array, then find.since the "Arrays.sort(args)" uses merge sort, the rc is O(nlg(n)) and sc O(1).
 3. use hashset, rc O(n) but sc O(n) as well.
+
+ */ 
+// or checkout how stream() makes your java 8 times slower:
+  public boolean containsDuplicate(int[] nums) {
+
+        return nums.length != Arrays.stream(nums)
+                .distinct()
+                .count();
+    }
