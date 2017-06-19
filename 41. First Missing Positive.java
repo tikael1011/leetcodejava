fb2a8cf2145c82d85dbb70fcb17350693bd5de6e
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-19 10:25:15 -0400

    Hard

--- 41. First Missing Positive.java
+++ 41. First Missing Positive.java
@@ -8,3 +8,26 @@ and [3,4,-1,1] return 2.
 Your algorithm should run in O(n) time and uses constant space.
 
 */
+
+//I guess this is actually medium difficulty?
+
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i]-1] != nums[i]) {
+                swap(nums, i, nums[i]-1);
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != i+1)    return i+1;
+        }
+        return nums.length+1;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
