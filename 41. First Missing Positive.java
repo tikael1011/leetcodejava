e6e5f4d3de1e5da21ee2157e8f9ac608f4c89673
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-19 10:26:27 -0400

    Hard

--- 41. First Missing Positive.java
+++ 41. First Missing Positive.java
@@ -11,6 +11,12 @@ Your algorithm should run in O(n) time and uses constant space.
 
 //I guess this is actually medium difficulty?
 
+/*
+the key is that the answer must be in [1, n + 1].
+If n numbers are 1, 2, 3, ..., n, then the answer is n + 1.
+use a number x (x <= 0 || x > n) to replace a number in 1, 2, 3, ..., n. There must be a number less than n+1.
+*/
+
 public class Solution {
     public int firstMissingPositive(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
