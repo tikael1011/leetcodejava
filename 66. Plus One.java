7fe045fb0504e24144e0f02933c5c1e2095e1ba7
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-24 18:27:11 -0400

    Easy

---
+++ 66. Plus One.java
@@ -0,0 +1,36 @@
+/*
+Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+*/
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            
+            digits[i] = 0;
+        }
+        
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+        
+        return newNumber;
+    }
+}
+
+//python version
+// deal with the type conversion easily!
+
+class Solution(object):
+    def plusOne(self, digits):
+        num = 0
+        for i in range(len(digits)):
+        	num = num*10 + digits[i]
+        return [int(i) for i in str(num+1)]
