fca4b1ababc7590bd9c2ce4f7784851881797f78
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-28 15:48:41 -0400

    Easy

--- 219. Contains Duplicate II.java
+++ 219. Contains Duplicate II.java
@@ -2,3 +2,28 @@
 Given an array of integers and an integer k, find out whether there are two distinct indices i and
 j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
 */
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+            Set<Integer> set = new HashSet<Integer>();
+            for(int i = 0; i < nums.length; i++){
+                if(i > k) set.remove(nums[i-k-1]);
+                if(!set.add(nums[i])) return true;
+            }
+            return false;
+     }
+}
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dic = {}
+        for i, v in enumerate(nums):
+            if v in dic and i - dic[v] <= k:
+                return True
+            dic[v] = i
+        return False
