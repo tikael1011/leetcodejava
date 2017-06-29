72f76cca64b307dcb06d0404fd310bc50f79269e
tikael1011 <tikael1011@users.noreply.github.com>
2017-06-29 02:44:27 -0400

    Easy

--- 263. Ugly Number.java
+++ 263. Ugly Number.java
@@ -6,3 +6,18 @@ For example, 6, 8 are ugly while 14 is not ugly since it includes another prime 
 
 Note that 1 is typically treated as an ugly number.
 */
+
+for (int i=2; i<6 && num>0; i++)
+    while (num % i == 0)
+        num /= i;
+return num == 1;
+
+
+public boolean isUgly(int num) {
+    if(num==1) return true;
+    if(num==0) return false;
+    while(num%2==0) num=num/2;
+    while(num%3==0) num=num/3;
+    while(num%5==0) num=num/5;
+    return num==1;
+}
