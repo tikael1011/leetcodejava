78089877a84334470231fc53efa00c2f25cdcaba
tikael1011 <tikael1011@users.noreply.github.com>
2017-07-06 19:21:13 -0400

    Medium

---
+++ 621. Task Scheduler.java
@@ -0,0 +1,55 @@
+/*
+Given a char array representing tasks CPU need to do. It contains capital letters A to Z 
+where different letters represent different tasks.Tasks could be done without original order.
+Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
+
+However, there is a non-negative cooling interval n that means between two same tasks, 
+there must be at least n intervals that CPU are doing different tasks or just be idle.
+
+You need to return the least number of intervals the CPU will take to finish all the given tasks.
+
+Example 1:
+Input: tasks = ['A','A','A','B','B','B'], n = 2
+Output: 8
+Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
+Note:
+The number of tasks is in the range [1, 10000].
+The integer n is in the range [0, 100].
+
+*/
+
+// to better understand this problem, every task is an interval. 8 is from 6 tasks and two 'idles'.
+
+// (c[25] - 1) * (n + 1) + 25 - i  is frame size
+// when inserting chars, the frame might be "burst", then tasks.length takes precedence
+// when 25 - i > n, the frame is already full at construction, the following is still valid.
+public class Solution {
+    public int leastInterval(char[] tasks, int n) {
+
+        int[] c = new int[26];
+        for(char t : tasks){
+            c[t - 'A']++;
+        }
+        Arrays.sort(c);
+        int i = 25;
+        while(i >= 0 && c[i] == c[25]) i--;
+
+        return Math.max(tasks.length, (c[25] - 1) * (n + 1) + 25 - i);
+    }
+}
+
+
+// another idea is to 
+//https://discuss.leetcode.com/topic/92866/java-priorityqueue-
+//solution-similar-problem-rearrange-string-k-distance-apart
+
+/*
+or python version ,take advantage of collection.Counter library
+
+def leastInterval(self, tasks, N):
+    task_counts = collections.Counter(tasks).values()
+    M = max(task_counts)
+    Mct = task_counts.count(M)
+    return max(len(tasks), (M - 1) * (N + 1) + Mct)
+
+*/
