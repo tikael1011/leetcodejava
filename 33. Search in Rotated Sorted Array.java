483e6a1f2b7fc354353cec8b80b21aebd1e5fd0a
tikael1011 <tikael1011@users.noreply.github.com>
2017-07-23 21:07:42 -0400

    Medium[er]
    binary search

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -9,3 +9,34 @@ You may assume no duplicate exists in the array.
 
 
 */
+
+
+// idea is binary search, of course modify a little bit.
+
+//core logic
+/*
+        if not nums:
+            return -1
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) / 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
+
+
+*/
