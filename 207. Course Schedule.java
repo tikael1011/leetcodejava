f97a713097765147b7553aae878c2cda13c8eca2
tikael1011 <tikael1011@users.noreply.github.com>
2017-07-25 00:38:21 -0400

    UP[i][p] Medium
    topological sort

--- 207. Course Schedule.java
+++ 207. Course Schedule.java
@@ -79,3 +79,46 @@ public class Solution {
         return false;
     }
 }
+
+
+/*
+Another OO thought
+*/
+
+/*
+public boolean canFinish(int numCourses, int[][] prerequisites) {
+    Course[] courses = new Course[numCourses];
+    for (int i = 0; i < numCourses; i++) {
+        courses[i] = new Course();
+    }
+    for (int i = 0; i < prerequisites.length; i++) {
+        courses[prerequisites[i][0]].add(courses[prerequisites[i][1]]);
+    }
+    for (int i = 0; i < numCourses; i++) {
+        if(isCyclic(courses[i])) return false;
+    }
+    return true;
+}
+
+private boolean isCyclic(Course cur) {
+    if (cur.tested == true) return false;
+    if (cur.visited == true) return true;
+    cur.visited = true;
+    for (Course c : cur.pre) {
+        if (isCyclic(c)) {
+            return true;
+        }
+    }
+    cur.tested = true;
+    return false;
+}
+
+class Course {
+    boolean visited = false;
+    boolean tested = false;
+    List<Course> pre = new ArrayList<Course>();
+    public void add(Course c) {
+        pre.add(c);
+    }
+}
+*/
