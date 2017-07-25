60603844272e8c778c505022d0db9ac12b40c731
tikael1011 <Qian@Aeoluss-MacBook-Pro.local>
2017-07-25 00:27:36 -0400

    Medium[p]

--- 207. Course Schedule.java
+++ 207. Course Schedule.java
@@ -52,4 +52,30 @@ public class Solution {
     }
 }
 
-// dfs maybe a little bit easier to understand?
+// dfs maybe a little bit easier to understand? recursive
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] visited = new int[numCourses];
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0 && dfsDetectCycle(graph, visited, i)) return false;// 0: White
+        }
+        return true;
+    }
+    
+    private boolean dfsDetectCycle(List<List<Integer>> graph, int[] visited, int prere) {
+        visited[prere] = -1; //-1: Gray
+        for (Integer course: graph.get(prere)) {
+            if (visited[course] == -1) return true;
+            else if (visited[course] == 0 && dfsDetectCycle(graph, visited, course)) return true;
+        }
+        visited[prere] = 1; //1: Black
+        return false;
+    }
+}
