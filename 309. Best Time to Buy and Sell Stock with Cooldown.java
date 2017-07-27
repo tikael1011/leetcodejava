f4b541cfb161821d03a33bc80d3c57f217cee4f8
tikael1011 <tikael1011@users.noreply.github.com>
2017-07-26 22:02:54 -0400

    Medium[i][p]
    DP

--- 309. Best Time to Buy and Sell Stock with Cooldown.java
+++ 309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -12,3 +12,32 @@ prices = [1, 2, 3, 0, 2]
 maxProfit = 3
 transactions = [buy, sell, cooldown, buy, sell]
 */
+
+/*
+buy[i] : Maximum profit which end with buying on day i or end
+with buying on a day before i and takes rest until the day i since then.
+
+sell[i] : Maximum profit which end with selling on day i or end
+with selling on a day before i and takes rest until the day i since then.
+
+Let b2, b1, b0 represent buy[i - 2], buy[i - 1], buy[i]
+Let s2, s1, s0 represent sell[i - 2], sell[i - 1], sell[i]
+*/
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 1) return 0;
+
+        int b0 = -prices[0], b1 = b0;
+        int s0 = 0, s1 = 0, s2 = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            b0 = Math.max(b1, s2 - prices[i]);
+            s0 = Math.max(s1, b1 + prices[i]);
+            b1 = b0; s2 = s1; s1 = s0; // move forward
+        }
+        return s0;
+    }
+}
+// there are other DP thougts, or O(n) sc thoughts, try to understand every single one is not that
+//necessarily, IMO, but I will try to understand at least 2.
