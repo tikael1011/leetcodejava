560904f483c35c9b023ae5cdff98b963b7b82331
tikael1011 <Qian@Aeoluss-MacBook-Pro.local>
2017-08-01 20:10:06 -0400

    Medium[er]
    Binary Search

--- 162. Find Peak Element.java
+++ 162. Find Peak Element.java
@@ -28,3 +28,17 @@ class Solution(object):
                 right = mid-1
         return left if nums[left] >= nums[right] else right
 */
+
+public int findPeakElement(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+    while(left < right) {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] < nums[mid + 1]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return left;
+}
