d684b99348a89e7fc0ca0488720c4a99684f0fe4
tikael1011 <tikael1011@users.noreply.github.com>
2017-08-05 17:52:00 -0400

    Easy
    two pointers

---
+++ 345. Reverse Vowels of a String.java
@@ -0,0 +1,50 @@
+/*
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+
+Note:
+The vowels does not include the letter "y".
+*/
+
+
+//use a TWO pointer method, Java one-pass
+public String reverseVowels(String s) {
+    if(s == null || s.length()==0) return s;
+    String vowels = "aeiouAEIOU";
+    char[] chars = s.toCharArray();
+    int start = 0;
+    int end = s.length()-1;
+    while(start<end){
+        
+        while(start<end && !vowels.contains(chars[start]+"")){
+            start++;
+        }
+        
+        while(start<end && !vowels.contains(chars[end]+"")){
+            end--;
+        }
+        
+        char temp = chars[start];
+        chars[start] = chars[end];
+        chars[end] = temp;
+        
+        start++;
+        end--;
+    }
+    return new String(chars);
+}
+
+
+/*
+Python Two Passes
+
+def reverseVowels(self, s):
+    vowels = re.findall('(?i)[aeiou]', s)
+    return re.sub('(?i)[aeiou]', lambda m: vowels.pop(), s)
+
+*/
