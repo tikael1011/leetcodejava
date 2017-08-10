7400c33600cd50e4bf4b4ab0ddc87c6f52beaf96
tikael1011 <tikael1011@users.noreply.github.com>
2017-08-09 22:13:41 -0400

    Medium

--- 477. Total Hamming Distance.java
+++ 477. Total Hamming Distance.java
@@ -15,3 +15,17 @@ Note:
 Elements of the given array are in the range of 0 to 10^9
 Length of the array will not exceed 10^4.
 */
+
+//if there are n integers in the array and k of them have a particular bit set and (n-k) do not,
+//then that bit contributes k*(n-k) hamming distance to the total.
+
+public int totalHammingDistance(int[] nums) {
+    int total = 0, n = nums.length;
+    for (int j=0;j<32;j++) {
+        int bitCount = 0;
+        for (int i=0;i<n;i++) 
+            bitCount += (nums[i] >> j) & 1; // if the range contains negative values, use >>> instead
+        total += bitCount*(n - bitCount);
+    }
+    return total;
+}
