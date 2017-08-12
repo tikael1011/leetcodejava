50ce24d78d7b8690b9379073d21343a58cfd400a
tikael1011 <tikael1011@users.noreply.github.com>
2017-08-11 21:50:24 -0400

    Medium

---
+++ 145. Binary Tree Postorder Traversal.java
@@ -0,0 +1,56 @@
+/*
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [3,2,1].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+*/
+
+//remind me the use of Deque(double end queue) and other method in it,
+//whether return false/null or throws exception.
+
+
+public List<Integer> postorderTraversal(TreeNode root) {
+	LinkedList<Integer> ans = new LinkedList<>();
+	Stack<TreeNode> stack = new Stack<>();
+	if (root == null) return ans;
+	
+	stack.push(root);
+	while (!stack.isEmpty()) {
+		TreeNode cur = stack.pop();
+		ans.addFirst(cur.val);
+		if (cur.left != null) {
+			stack.push(cur.left);
+		}
+		if (cur.right != null) {
+			stack.push(cur.right);
+		} 
+	}
+	return ans;
+}
+
+
+//with reverse
+
+public List<Integer> postorderTraversal(TreeNode root) {
+	List<Integer> results = new ArrayList<Integer>();
+	Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+	while (!stack.isEmpty() || root != null) {
+		if (root != null) {
+			stack.push(root);
+			results.add(root.val);
+			root = root.right;
+		} else {
+			root = stack.pop().left;
+		}
+	}
+	Collections.reverse(results);
+	return results;
+}
