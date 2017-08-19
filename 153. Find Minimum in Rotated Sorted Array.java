2fdab6996d4ed24836036d7cd9b1f3b3894080e6
tikael1011 <tikael1011@users.noreply.github.com>
2017-08-19 19:26:12 -0400

    Medium[er]

---
+++ 153. Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,26 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+*/
+
+
+//It happens to be a standard binary search problem.
+//if nums[mid] < nums[right], min is in the left part, and otherwise in the right part.
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] < nums[high]) high = mid;
+            else low = mid + 1;
+        }
+        return nums[low];
+    }
+}
