504a1b1f4cb8507b6533bdf5a076e34133d3222d
tikael1011 <tikael1011@users.noreply.github.com>
2017-08-20 19:03:55 -0400

    Medium
    dp

--- 64. Minimum Path Sum.java
+++ 64. Minimum Path Sum.java
@@ -2,3 +2,21 @@
 Given a m x n grid filled with non-negative numbers, find a path 
 from top left to bottom right which minimizes the sum of all numbers along its path
 */
+
+//classis dp prolem
+//again, is altering allowed/good? if so, O(1) space, otherwise O(n) space.
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+            if(i == 0 && j != 0) grid[i][j] += grid[i][j-1];
+            if(i != 0 && j == 0) grid[i][j] += grid[i-1][j];
+            if (i != 0 && j != 0) grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+}
