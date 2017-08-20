acc98fe01d1b6a4567a04e251c09d45ad17f5828
tikael1011 <tikael1011@users.noreply.github.com>
2017-08-20 01:06:06 -0400

    Hard
    DP

--- 174. Dungeon Game.java
+++ 174. Dungeon Game.java
@@ -23,3 +23,25 @@ The knight's health has no upper bound.
 Any room can contain threats or power-ups, even the first room the knight enters and the bottom-right
 room where the princess is imprisoned.
 */
+
+//classic DP problem
+
+//O(mn) rc, O(1) space, but the following method alters the input, if no in space requirement,
+//use a 2D array instead
+
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int row = dungeon.length;
+        int col = dungeon[0].length;
+        for(int i = row- 1; i >= 0; i--) {       
+            for(int j = col - 1; j >= 0; j--) {
+                if(i == (row - 1) && j == (col - 1)) dungeon[i][j] = Math.max(1, 1 - dungeon[i][j]); // begining
+                else if(i == (row - 1)) dungeon[i][j] = Math.max(1, dungeon[i][j+1] - dungeon[i][j]);//bottum boundary
+                else if(j == (col - 1)) dungeon[i][j] = Math.max(1, dungeon[i+1][j] - dungeon[i][j]);//right boundary
+                else dungeon[i][j] = Math.max(1, Math.min(dungeon[i + 1][j], dungeon[i][j + 1]) - dungeon[i][j]);
+            }
+        }
+        
+    return dungeon[0][0];
+    }
+}
