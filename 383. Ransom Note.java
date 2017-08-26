5c847e1edafe0894f8094e21d5602ab547f5d899
tikael1011 <tikael1011@users.noreply.github.com>
2017-08-25 21:28:00 -0400

    Easy

---
+++ 383. Ransom Note.java
@@ -0,0 +1,33 @@
+/*
+Given an arbitrary ransom note string and another string containing letters from all the magazines,
+write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+*/
+
+//the basic idea is using Counting sort, since we only have at most 26 letters to counter in MAGAZINE
+//then check the NOTE, minus onece or each encounter, if counter < 0, return false
+
+／／O(m+n) rc ,O(1)sc, hashmap is another implementation.
+
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(--arr[ransomNote.charAt(i)-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
