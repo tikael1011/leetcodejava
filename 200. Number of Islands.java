4539be674e5fd0bde054a4a999fefd7a0abda33d
tikael1011 <tikael1011@users.noreply.github.com>
2017-09-10 17:36:16 -0700

    Medium

---
+++ 200. Number of Islands.java
@@ -0,0 +1,69 @@
+/*
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+11110
+11010
+11000
+00000
+Answer: 1
+
+Example 2:
+
+11000
+11000
+00100
+00011
+*/
+
+//This question is about DFS and better use a helper function, otherwise, keep track of i,j in a nested for loop will be
+//pain ***.
+
+/*
+Python
+
+def numIslands(self, grid):
+    def sink(i, j):
+        if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+            grid[i][j] = '0'
+            map(sink, (i+1, i-1, i, i), (j, j, j+1, j-1))
+            return 1
+        return 0
+    return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
+*/
+
+//The java solution alters the input, we can creat a 2d matrix = input grid, but this will cost extra space,
+// since take the stack space into considersation ,it is already O(n), does not matter that much. is it?
+// The rc is O(mn), where m & n are the length of row & cloumn.
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void clearRestOfLand(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') return;
+        
+        grid[i][j] = '0'; //reset to avoid duplicate
+        
+        // four direction search, it is required, only right and down is not enough.
+        
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+        return;
+    }
+}
+
