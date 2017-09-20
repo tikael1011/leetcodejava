15d3c25f1f6049cba2272917c7053af10871a774
tikael1011 <tikael1011@users.noreply.github.com>
2017-09-20 14:54:37 -0700

    Medium

---
+++ 71. Simplify Path.java
@@ -0,0 +1,29 @@
+／*
+Given an absolute path for a file (Unix-style), simplify it.
+
+For example,
+path = "/home/", => "/home"
+path = "/a/./b/../../c/", => "/c"
+click to show corner cases.
+
+Corner Cases:
+Did you consider the case where path = "/../"?
+In this case, you should return "/".
+Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+In this case, you should ignore redundant slashes and return "/home/foo".
+
+*／
+
+//the idea can be easily seen from the code, STACK, just do not forget to deal with some corner case.
+
+class Solution(object):
+    def simplifyPath(self, path):
+        places = [p for p in path.split("/") if p!="." and p!=""]
+        stack = []
+        for p in places:
+            if p == "..":
+                if len(stack) > 0:
+                    stack.pop()
+            else:
+                stack.append(p)
+        return "/" + "/".join(stack)
