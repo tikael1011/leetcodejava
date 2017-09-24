ebc4bbc22e6f7a362bc7b16c73cf1110048bbeb3
tikael1011 <tikael1011@users.noreply.github.com>
2017-09-23 20:05:41 -0700

    Easy[er]

---
+++ 266. Palindrome Permutation.java
@@ -0,0 +1,12 @@
+/*
+Given a string, determine if a permutation of the string could form a palindrome.
+
+For example,
+"code" -> False, "aab" -> True, "carerac" -> True.
+*/
+
+//https://leetcode.com/articles/palindrome-permutation/
+//There is one flaw in the above link, the algorithm is right though.
+
+//I will prefer the array method or Pyhthon counter method,
+//hashmap/hashset is kinda redundant, since we do not care about the O(1) lookup time.
