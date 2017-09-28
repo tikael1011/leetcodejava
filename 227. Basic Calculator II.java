ad36f8974697e84f98f3480dd219292f875b7f1f
tikael1011 <tikael1011@users.noreply.github.com>
2017-09-28 14:05:11 -0700

    Medium

--- 227. Basic Calculator II.java
+++ 227. Basic Calculator II.java
@@ -1,1 +1,57 @@
+/*
+Implement a basic calculator to evaluate a simple expression string.
 
+The expression string contains only non-negative integers, +, -, *, / operators and empty spaces .
+The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid.
+
+Some examples:
+"3+2*2" = 7
+" 3/2 " = 1
+" 3+5 / 2 " = 5
+Note: Do not use the eval built-in library function.
+*/
+
+//After basic calculator I, this one is easier, the idea is to use a stack, rc O(n), sc worst case is O(n).
+
+//but there is always an alternative way, aka. calc * and / first, but this will result in at least two for loops.
+//or atucally we can avoid using a stack. this can achieve sc O(1) and rc O(n).
+
+
+class Solution {
+    public int calculate(String s) {
+        int len;
+        if(s==null || (len = s.length())==0) return 0;
+        Stack<Long> stack = new Stack<Long>();
+        long num = 0;
+        char sign = '+';
+        for(int i=0;i<len;i++){
+            if(Character.isDigit(s.charAt(i))){
+                num = num*10+s.charAt(i)-'0';
+            }
+            if((!Character.isDigit(s.charAt(i)) &&' '!=s.charAt(i)) || i==len-1){
+                if(sign=='-'){
+                    stack.push(-num);
+                }
+                if(sign=='+'){
+                    stack.push(num);
+                }
+                if(sign=='*'){
+                    stack.push(stack.pop()*num);
+                }
+                if(sign=='/'){
+                    stack.push(stack.pop()/num);
+                }
+                sign = s.charAt(i);
+                num = 0;
+            }
+        }
+
+        long res = 0;
+        for(long nums:stack){
+            res += nums;
+        }
+        return res <= Integer.MAX_VALUE ? (int)res : null;
+    }
+}
