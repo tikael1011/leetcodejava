179e30dc77a776db814f2c42a7f5e4ea8ea002ed
tikael1011 <tikael1011@users.noreply.github.com>
2017-10-10 23:52:44 -0400

    Hard

---
+++ 32. Longest Valid Parentheses.java
@@ -0,0 +1,55 @@
+/*
+Given a string containing just the characters '(' and ')', 
+find the length of the longest valid (well-formed) parentheses substring.
+
+For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+*/
+
+
+
+//Two main solutions, one is using stack, the other is DP
+//and for the push solution, we can push either '(' or the index
+//and I believe the index one is better
+
+
+public class Solution {
+    public int longestValidParentheses(String s) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        int result = 0;
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')' && stack.size() > 1 && s.charAt(stack.peek()) == '(') {
+                stack.pop();
+                result = Math.max(result, i - stack.peek());
+            } else {
+                stack.push(i);
+            }
+        }
+        return result;
+    }
+}
+
+//dp one , faster
+//from the others
+
+public int longestValidParentheses(String s) {
+    char[] S = s.toCharArray();
+    int[] V = new int[S.length];
+    int open = 0;
+    int max = 0;
+    for (int i=0; i<S.length; i++) {
+        if (S[i] == '(') open++;
+        if (S[i] == ')' && open > 0) {
+            // matches found
+            V[i] = 2+ V[i-1];
+            // add matches from previous
+            if(i-V[i]>0)
+                V[i] += V[i-V[i]];
+            open--;
+        }
+        if (V[i] > max) max = V[i];
+    }
+    return max;
+}
