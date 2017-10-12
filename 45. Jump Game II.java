81ee7e396134b3fdf214addce084962f19121f6d
tikael1011 <tikael1011@users.noreply.github.com>
2017-10-11 20:43:22 -0400

    Hard[er]

---
+++ 45. Jump Game II.java
@@ -0,0 +1,37 @@
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+For example:
+Given array A = [2,3,1,1,4]
+
+The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+
+Note:
+You can assume that you can always reach the last index.
+
+*/
+
+//as mentioned, the array is NON-negative int, so regardless note, there is always at least one path/solution.
+//The problem is kinda like to find a path in unweighted graph/tree
+
+class Solution {
+    public int jump(int[] A) {
+        int jumps = 0, curEnd = 0, curFarthest = 0;
+        for (int i = 0; i < A.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, i + A[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+
+                if (curEnd >= A.length - 1) {
+                    break;
+                }
+            }
+        }
+        return jumps;
+    }
+}
