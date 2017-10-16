72930b2ab3d243909245d5d311b9793233cb494c
tikael1011 <tikael1011@users.noreply.github.com>
2017-10-15 21:20:05 -0400

    Medium[i]

---
+++ 373. Find K Pairs with Smallest Sums.java
@@ -0,0 +1,60 @@
+/*
+You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+
+Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+
+Example 1:
+Given nums1 = [1,7,11], nums2 = [2,4,6],  k = 3
+
+Return: [1,2],[1,4],[1,6]
+
+The first 3 pairs are returned from the sequence:
+[1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+Given nums1 = [1,1,2], nums2 = [1,2,3],  k = 2
+
+Return: [1,1],[1,1]
+
+The first 2 pairs are returned from the sequence:
+[1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+Example 3:
+Given nums1 = [1,2], nums2 = [3],  k = 3 
+
+Return: [1,3],[2,3]
+
+All possible pairs are returned from the sequence:
+[1,3],[2,3]
+*/
+
+//Minheap, PriorityQueue
+
+//The idea is quite similar to 378. and the rc is O(klgk), lgk comes from the poll
+//check: http://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
+
+public class Solution {
+    public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]> que = new PriorityQueue<>((a,b)->a[0]+a[1]-b[0]-b[1]);
+        List<int[]> res = new ArrayList<>();
+        if(nums1.length==0 || nums2.length==0 || k==0) return res;
+        for(int i=0; i<nums1.length && i<k; i++) que.offer(new int[]{nums1[i], nums2[0], 0});
+        while(k-- > 0 && !que.isEmpty()){
+            int[] cur = que.poll();
+            res.add(new int[]{cur[0], cur[1]});
+            if(cur[2] == nums2.length-1) continue;
+            que.offer(new int[]{cur[0],nums2[cur[2]+1], cur[2]+1});
+        }
+        return res;
+    }
+}
+
+//And the following is the Python solution
+//from Stenfan
+
+//https://discuss.leetcode.com/topic/50450/slow-1-liner-to-fast-solutions
+
+
+//Another solution is O(k*m), where m is length of the shorter array. Since all the array are sorted, we can
+//come up with a more intuitivee solution, Two pointers, keep track of arr1 and arr2, (suppose arr1[0] <= arr2[0]), 
+//every time we move forward index1 in arr1, index2 in arr2 goes back to arr2[0].
