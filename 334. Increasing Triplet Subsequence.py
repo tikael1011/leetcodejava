3e62a837de1d6e489a3600a08ea41d28f9b3346f
tikael1011 <tikael1011@users.noreply.github.com>
2017-10-21 21:42:57 -0400

    Medium

---
+++ 334. Increasing Triplet Subsequence.py
@@ -0,0 +1,33 @@
+/*
+Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+
+Formally the function should:
+Return true if there exists i, j, k 
+such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+Your algorithm should run in O(n) time complexity and O(1) space complexity.
+
+Examples:
+Given [1, 2, 3, 4, 5],
+return true.
+
+Given [5, 4, 3, 2, 1],
+return false.
+*/
+
+//the idea is find smallest and middle one, and we can find the largest one, return true otherwise false
+
+//There is no NEED to track index or use 2/3 pointers.
+
+class Solution(object):
+    def increasingTriplet(self,nums):
+        first = second = float('inf')
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+        return False
+            
+        
