fb5b14f27f6c09f4c37ce6b38759e54d33a9d438
tikael1011 <tikael1011@users.noreply.github.com>
2017-10-25 01:50:06 -0400

    Medium
    dfs

---
+++ 494. Target Sum.java
@@ -0,0 +1,143 @@
+/*
+
+You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. 
+Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+Example 1:
+Input: nums is [1, 1, 1, 1, 1], S is 3. 
+Output: 5
+Explanation: 
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+There are 5 ways to assign symbols to make the sum of nums be target 3.
+Note:
+The length of the given array is positive and will not exceed 20.
+The sum of elements in the given array will not exceed 1000.
+Your output answer is guaranteed to be fitted in a 32-bit integer.
+
+*/
+
+public class Solution {
+    int result = 0;
+	
+    public int findTargetSumWays(int[] nums, int S) {
+        if (nums == null || nums.length == 0) return result;
+        helper(nums, S, 0, 0);
+        return result;
+    }
+    
+    public void helper(int[] nums, int target, int pos, long eval){
+        if (pos == nums.length) {
+            if (target == eval) result++;
+            return;
+        }
+        helper(nums, target, pos + 1, eval + nums[pos]);
+        helper(nums, target, pos + 1, eval - nums[pos]);
+    }
+}
+
+//shorter version
+
+/*
+public int findTargetSumWays(int[] nums, int S) {
+    return dfs(nums, 0, S, 0);
+}
+
+private int dfs(int[] nums, int sum, int target, int k) {
+    if (nums.length == k) {
+        return sum == target ? 1 : 0;
+    }
+    return dfs(nums, sum + nums[k], target, k + 1) + 
+            dfs(nums, sum - nums[k], target, k + 1);
+}
+*/
+
+//one optimisizon, earlier return
+
+public class Solution {
+    int result = 0;
+	
+    public int findTargetSumWays(int[] nums, int S) {
+        if(nums == null || nums.length == 0) return result;
+        
+        int n = nums.length;
+        int[] sums = new int[n];
+        sums[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+            sums[i] = sums[i + 1] + nums[i];
+        
+        helper(nums, sums, S, 0);
+        return result;
+    }
+    public void helper(int[] nums, int[] sums, int target, int pos){
+        if(pos == nums.length){
+            if(target == 0) result++;
+            return;
+        }
+        
+        if (sums[pos] < Math.abs(target)) return;
+        
+        helper(nums, sums, target + nums[pos], pos + 1);
+        helper(nums, sums, target - nums[pos], pos + 1);
+    }
+}
+
+//another op, momoization.
+
+public class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        return helper(nums, 0, 0, S, new HashMap<>());
+    }
+    private int helper(int[] nums, int index, int sum, int S, Map<String, Integer> map){
+        String encodeString = index + "->" + sum;
+        if (map.containsKey(encodeString)){
+            return map.get(encodeString);
+        }
+        if (index == nums.length){
+            if (sum == S){
+                return 1;
+            }else {
+                return 0;
+            }
+        }
+        int curNum = nums[index];
+        int add = helper(nums, index + 1, sum - curNum, S, map);
+        int minus = helper(nums, index + 1, sum + curNum, S, map);
+        map.put(encodeString, add + minus);
+        return add + minus;
+    }
+}
+
+
+//DP
+
+public class Solution {
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0; 
+        for(int i: nums) sum+=i;
+        if(s>sum || s<-sum) return 0;
+        int[] dp = new int[2*sum+1];
+        dp[0+sum] = 1;
+        for(int i = 0; i<nums.length; i++){
+            int[] next = new int[2*sum+1];
+            for(int k = 0; k<2*sum+1; k++){
+                if(dp[k]!=0){
+                    next[k + nums[i]] += dp[k];
+                    next[k - nums[i]] += dp[k];
+                }
+            }
+            dp = next;
+        }
+        return dp[sum+s];
+    }
+}
