683a708dee605c3ebc3a7b00e5a1128b538f9688
tikael1011 <tikael1011@users.noreply.github.com>
2017-11-02 22:58:23 -0400

    Hard[per]

---
+++ 352. Data Stream as Disjoint Intervals.java
@@ -0,0 +1,91 @@
+/*
+Given a data stream input of non-negative integers a1, a2, ..., an, ..., 
+summarize the numbers seen so far as a list of disjoint intervals.
+
+For example, suppose the integers from the data stream are 1, 3, 7, 2, 6, ..., then the summary will be:
+
+[1, 1]
+[1, 1], [3, 3]
+[1, 1], [3, 3], [7, 7]
+[1, 3], [7, 7]
+[1, 3], [6, 7]
+Follow up:
+What if there are lots of merges and the number of disjoint intervals are small compared to the data stream's size?
+*/
+
+//..Following is java setup, lgN
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+
+public class SummaryRanges {
+    TreeMap<Integer, Interval> tree;
+
+    public SummaryRanges() {
+        tree = new TreeMap<>();
+    }
+
+    public void addNum(int val) {
+        if(tree.containsKey(val)) return;
+        Integer l = tree.lowerKey(val);
+        Integer h = tree.higherKey(val);
+        if(l != null && h != null && tree.get(l).end + 1 == val && h == val + 1) {
+            tree.get(l).end = tree.get(h).end;
+            tree.remove(h);
+        } else if(l != null && tree.get(l).end + 1 >= val) {
+            tree.get(l).end = Math.max(tree.get(l).end, val);
+        } else if(h != null && h == val + 1) {
+            tree.put(val, new Interval(val, tree.get(h).end));
+            tree.remove(h);
+        } else {
+            tree.put(val, new Interval(val, val));
+        }
+    }
+
+    public List<Interval> getIntervals() {
+        return new ArrayList<>(tree.values());
+    }
+}
+
+//..Following is python setup.
+
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+
+class SummaryRanges(object):
+
+  def __init__(self):
+    self.intervals = []
+  
+  def __lt__(self, other):
+    if isinstance(other, self.__class__):
+        return self.distance < other.distance
+    return NotImplemented
+  
+  def addNum(self, val):
+    heapq.heappush(self.intervals, (val, Interval(val, val)))
+    
+  def getIntervals(self):
+    stack = []
+    while self.intervals:
+        idx, cur = heapq.heappop(self.intervals)
+        if not stack:
+            stack.append((idx, cur))
+        else:
+            _, prev = stack[-1]
+            if prev.end + 1 >= cur.start:
+                prev.end = max(prev.end, cur.end)
+            else:
+                stack.append((idx, cur))
+    self.intervals = stack
+    return list(map(lambda x: x[1], stack))
