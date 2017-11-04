887ce17dbea7830c301bc6610b00f24cf54b34d7
tikael1011 <tikael1011@users.noreply.github.com>
2017-11-03 22:16:47 -0400

    Medium

---
+++ 173. Binary Search Tree Iterator.java
@@ -0,0 +1,60 @@
+/*
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+*/
+
+//inorder tranversal idea
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class BSTIterator {
+
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        fillStack(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode curNode = stack.pop();
+        fillStack(curNode.right); // O(n)/n = 1
+        return curNode.val;
+    }
+
+    private void fillStack(TreeNode root){
+        while(root != null){
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+ 
+ 
+ // https://discuss.leetcode.com/topic/6604/ideal-solution-using-stack-java
+ //above is the stack thought.
