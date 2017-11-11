d705727070c7bb5de8fb7872c9b3cccfacfada96
tikael1011 <tikael1011@users.noreply.github.com>
2017-11-10 23:12:06 -0500

    Hard

--- 23. Merge k Sorted Lists.java
+++ 23. Merge k Sorted Lists.java
@@ -1,3 +1,107 @@
 /*
 Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
 */
+
+//there are two main, or three, if you take brute force into consideration, thoughts,
+//One is to build a k size heap(priority queue)
+//the other is to think of merge thought and both of them rc is O(NlgK).
+
+//here is a solution. IMHO, at least the bruteforce one should not
+//be accepted b/c the huge memory consumption. if you can't load all the nodes
+//into memory, your algorithm breaks.
+
+//merge solution:
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public static ListNode mergeKLists(ListNode[] lists){
+        return partion(lists,0,lists.length-1);
+    }
+
+    public static ListNode partion(ListNode[] lists,int s,int e){
+        if(s==e)  return lists[s];
+        if(s<e){
+            int q=(s+e)/2;
+            ListNode l1=partion(lists,s,q);
+            ListNode l2=partion(lists,q+1,e);
+            return merge(l1,l2);
+        }else
+            return null;
+    }
+
+    //This function is from Merge Two Sorted Lists.
+    public static ListNode merge(ListNode l1,ListNode l2){
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        if(l1.val<l2.val){
+            l1.next=merge(l1.next,l2);
+            return l1;
+        }else{
+            l2.next=merge(l1,l2.next);
+            return l2;
+        }
+    }
+}
+
+/*
+another implementation
+from : https://discuss.leetcode.com/topic/2780/a-java-solution-based-on-priority-queue/5?page=1
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head=null;
+        ListNode former=null;
+        while (l1!=null&&l2!=null) {
+            if (l1.val>l2.val) {
+                if (former==null) former=l2; else former.next=l2;
+                if (head==null) head=former; else former=former.next;
+                l2=l2.next;
+            } else {
+                if (former==null) former=l1; else former.next=l1;
+                if (head==null) head=former; else former=former.next;
+                l1=l1.next;
+            }
+        }
+        if (l2!=null) l1=l2;
+        former.next=l1;
+        return head;
+        
+    }
+    
+    public ListNode mergeKLists(List<ListNode> lists) {
+        if (lists.size()==0) return null;
+        if (lists.size()==1) return lists.get(0);
+        if (lists.size()==2) return mergeTwoLists(lists.get(0), lists.get(1));
+        return mergeTwoLists(mergeKLists(lists.subList(0, lists.size()/2)), 
+            mergeKLists(lists.subList(lists.size()/2, lists.size())));
+    }
+}
+*/
+
+/*
+pq/heap
+
+from Queue import PriorityQueue
+class Solution(object):
+    def mergeKLists(self, lists):
+        dummy = ListNode(None)
+        curr = dummy
+        q = PriorityQueue()
+        for node in lists:
+            if node: q.put((node.val,node))
+        while q.qsize()>0:
+            curr.next = q.get()[1]
+            curr=curr.next
+            if curr.next: q.put((curr.next.val, curr.next))
+        return dummy.next
+*/
