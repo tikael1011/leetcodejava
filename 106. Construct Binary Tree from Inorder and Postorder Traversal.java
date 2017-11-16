fff8cbe21c52a3d2df5c98bdf4e0d56df28ccd1d
tikael1011 <tikael1011@users.noreply.github.com>
2017-11-16 16:32:48 -0500

    Medium

--- 106. Construct Binary Tree from Inorder and Postorder Traversal.java
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -4,3 +4,16 @@ Given inorder and postorder traversal of a tree, construct the binary tree.
 Note:
 You may assume that duplicates do not exist in the tree.
 */
+
+//The idea is similar to 105, find the very root, split the inorder, then recursively all function.
+//some optimal are just ignored.
+
+/*
+def buildTree(self, inorder, postorder):
+    if inorder:
+        ind = inorder.index(postorder.pop())
+        root = TreeNode(inorder[ind])
+        root.right = self.buildTree(inorder[ind+1:], postorder)
+        root.left = self.buildTree(inorder[:ind], postorder)
+        return root
+*/
