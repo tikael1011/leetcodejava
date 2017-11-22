9ef65d7cddb43706312874dc2d34f0e1d5aa9c8f
tikael1011 <tikael1011@users.noreply.github.com>
2017-11-21 22:02:03 -0500

    Medium[er]

---
+++ 150. Evaluate Reverse Polish Notation.java
@@ -0,0 +1,42 @@
+/*
+
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Some examples:
+  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+*/
+
+// nothing fancy or new, use a stack,
+
+class Solution {
+    public int evalRPN(String[] a) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < a.length; i++) {
+        switch (a[i]) {
+            case "+":
+                stack.push(stack.pop() + stack.pop());
+                break;
+
+            case "-":
+                stack.push(-stack.pop() + stack.pop());
+                break;
+
+            case "*":
+                stack.push(stack.pop() * stack.pop());
+                break;
+
+            case "/":
+                int n1 = stack.pop(), n2 = stack.pop();
+                stack.push(n2 / n1);
+                break;
+
+            default:
+                stack.push(Integer.parseInt(a[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
