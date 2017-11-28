75319c31f8f4b67c57036b300040d7f6ced02e13
tikael1011 <tikael1011@users.noreply.github.com>
2017-11-28 18:52:45 -0500

    Easy

---
+++ 412. Fizz Buzz.java
@@ -0,0 +1,45 @@
+/*
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+For numbers which are multiples of both three and five output “FizzBuzz”.
+*/
+
+// The most easy % one is trivia.
+
+/*
+python:
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        return [str(i) * (i % 3 != 0 and i % 5 != 0) + "Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) 
+                for i in range(1, n + 1)]
+
+*/
+
+// while if % is not allowed, then:
+// and the following solution should be faster since % costs more than +
+
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ret = new ArrayList<String>(n);
+        for(int i=1,fizz=0,buzz=0;i<=n ;i++){
+            fizz++;
+            buzz++;
+            if(fizz==3 && buzz==5){
+                ret.add("FizzBuzz");
+                fizz=0;
+                buzz=0;
+            }else if(fizz==3){
+                ret.add("Fizz");
+                fizz=0;
+            }else if(buzz==5){
+                ret.add("Buzz");
+                buzz=0;
+            }else{
+                ret.add(String.valueOf(i));
+            }
+        } 
+        return ret;
+    }
+}
