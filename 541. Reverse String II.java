945f51a39adb30fa241dd7a659052fd47c67c2bf
tikael1011 <tikael1011@users.noreply.github.com>
2017-12-04 00:48:50 -0500

    Easy[er]

--- 541. Reverse String II.java
+++ 541. Reverse String II.java
@@ -10,3 +10,25 @@ Restrictions:
 The string consists of lower English letters only.
 Length of the given string and k will in the range [1, 10000]
 */
+
+
+//nothing fancy
+
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i += 2 * k) {
+            rev(ch, i, i + k);
+        }
+        return String.valueOf(ch);
+    }
+    
+    private void rev(char[] ch, int i, int j) {
+        j = Math.min(ch.length, j) - 1;
+        for (; i < j; i++, j--) {
+            char tmp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = tmp;
+        }
+    }
+}
